1da4cfc8cc7c6704055418c104979551c00f1c46
quaycei <quayce@timsle.com>
2017-04-11 13:17:16 -0400

    Update settings.py

--- timsleb/settings.py
+++ timsleb/settings.py
@@ -81,21 +81,20 @@ WSGI_APPLICATION = 'timsleb.wsgi.application'
 
 #Database
 #https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'timsleb',
-        'USER': 'postgres',
-        'PASSWORD': 'timsleadmin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'timsleb',
+#        'USER': 'postgres',
+#        'PASSWORD': 'timsleadmin',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 
-
-#DATABASES =  {'default' : dj_database_url.config(
-#                default='postgres://uheevssuwxvefl:33648d4bf8c96f3817eb5b3a486df711b2a3634f0fe99fc75a558c964158624c@ec2-54-235-177-45.compute-1.amazonaws.com:5432/dbd5hf6qkau82e')}
+DATABASES =  {'default' : dj_database_url.config(
+                default='postgres://uheevssuwxvefl:33648d4bf8c96f3817eb5b3a486df711b2a3634f0fe99fc75a558c964158624c@ec2-54-235-177-45.compute-1.amazonaws.com:5432/dbd5hf6qkau82e')}
 
 
 # Enable Connection Pooling
