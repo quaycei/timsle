5d5cc639e91ef946deb8e74c67347ff55938149c
Quayce Thomas <quayce@timsle.co>
2017-05-22 12:33:37 -0400

    modify signup

--- timsleb/settings.py
+++ timsleb/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
     'ordered_model',
 ]
 
